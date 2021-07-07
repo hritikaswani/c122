ef1ba502007cb66aef4b3d52f26a4a93e3d2303a
Hritik <65850052+hritikaswani@users.noreply.github.com>
2021-07-07 16:04:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,40 +1,31 @@
-import cv2
-import time
-import numpy as np
-
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-outputfile = cv2.VideoWriter("Output.avi", fourcc, 20.0, (640, 480))
-cap = cv2.VideoCapture(0)
-time.sleep(2)
-
-bg = 0
-for i in range(60):
-    ret, bg = cap.read()
-bg = np.flip(bg, axis = 1)
-
-while(cap.isOpened()):
-    ret, img = cap.read()
-    if not ret:
-        break
-    img = np.flip(img, axis = 1)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+import cv2  
+import numpy as np  
+  
+video = cv2.VideoCapture(0) 
+image = cv2.imread("me.jpeg") 
+  
+while True: 
+  
+    ret, frame = video.read() 
+    print(frame)
+    frame = cv2.resize(frame, (600, 400)) 
+    image = cv2.resize(image, (600, 400)) 
+  
+  
     u_black = np.array([104, 153, 70]) 
-    l_black = np.array([30, 30, 0])
-    mask1 = cv2.inRange(hsv, l_black, u_black)
-    u_black = np.array([104, 153, 70]) 
-    l_black = np.array([30, 30, 0])
-    mask2 = cv2.inRange(hsv, l_black, u_black)
-    mask1 = mask1+mask2
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
-    mask2 = cv2.bitwise_not(mask1)
-    res1 = cv2.bitwise_and(img, img, mask = mask2)
-    res2 = cv2.bitwise_and(bg, bg, mask = mask1)
-    finaloutput = cv2.addWeighted(res1, 1, res2, 1, 0)
-    outputfile.write(finaloutput)
-    cv2.imshow("WOW!", finaloutput)
-    cv2.waitKey(1)
-
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    l_black = np.array([30, 30, 0]) 
+  
+    mask = cv2.inRange(frame, l_black, u_black) 
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
+  
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
+  
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+  
+video.release() 
+cv2.destroyAllWindows() 
